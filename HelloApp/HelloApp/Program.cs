bafd6ff47d9c77b5695e0793bbff992fa1049975
KarimGuzairov <k.guzairov@gmail.com>
2018-08-17 15:09:46 +0300

    Hello

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloApp
 {
@@ -10,7 +6,19 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+            Console.ReadKey();
+
+            int x = 45; // Значение, которое надо зашифровать - в двоичной форме 101101
+            int key = 102; //Пусть это будет ключ - в двоичной форме 1100110
+            int encrypt = x ^ key; //Результатом будет число 1001011 или 75
+            Console.WriteLine("Зашифрованное число: " + encrypt);
+
+            int decrypt = encrypt ^ key; // Результатом будет исходное число 45
+            Console.WriteLine("Расшифрованное число: " + decrypt);
+
             Console.ReadKey();
         }
     }
